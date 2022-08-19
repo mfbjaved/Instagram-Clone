75ccecb1ddeb9be55f5e32ec808eeab52422dca8
Madiha Javed <mfbjaved@gmail.com>
2022-08-18 20:15:35 -0700

    test

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import image from './images/cbc7174b4f05.png'
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+const leftMain = ReactDOM.createRoot(document.getElementById('LeftHome'));
+leftMain.render(
+  <><h1>Instagram Clone</h1><img src={image} /></>
 );
 
 // If you want to start measuring performance in your app, pass a function
